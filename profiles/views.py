ad6cdd43de6e76462148295f66e70e680a9459c8
rpf13 <rpf13@me.com>
2023-07-19 12:23:39 +0000

    feat: Add search feature to profiles app
    This commit will add the search feature to the profiles app. It will
    enable the possibility to search after:
    username, club

--- profiles/views.py
+++ profiles/views.py
@@ -10,8 +10,15 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.order_by('-created_at')
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.order_by('-created_at')
+    filter_backends = [
+        filters.SearchFilter
+    ]
+    search_fields = [
+        'owner__username',
+        'club',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -19,5 +26,5 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.order_by('-created_at')
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.order_by('-created_at')
