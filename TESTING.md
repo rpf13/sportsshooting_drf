5c63b3fb00267d8efe6e9937b44a6670aa4d1e0b
rpf13 <rpf13@me.com>
2023-08-14 12:50:25 +0000

    feat: Add python unit tests for Gun app
    This commit will add unit tests for the gun app. It will test CRUD
    functionality along with permissions.
    TESTING.md got updated with base info of unit testing.

--- TESTING.md
+++ TESTING.md
@@ -2,4 +2,36 @@
 
 Return back to the [README.md](README.md) file.
 
+
+
+
+## Automated Testing
+
+I have conducted a series of automated tests on my application.
+
+I have tested the "Matches" and the "Guns" app via unit test.
+- [Matches Unit Testing](/matches/tests.py)
+- [Guns Unit Testing](/guns/tests.py)
+
+I fully acknowledge and understand that, in a real-world scenario, an extensive set of additional tests would be more comprehensive. However, I wanted to include a few example tests for CRUD functionality and permissions.
+
+### Python (Unit Testing)
+
+I have used Django's built-in unit testing framework to test the application functionality.
+
+In order to run the tests, I ran the following command in the terminal each time:
+
+`python3 manage.py test`
+
+All testcases are successfully executed:
+
+![Unit Test Gun & Matches App](docs/testing/unit_matches_guns.png)
+
+The following table shows a summary of testcases executed:
+
+| Class | Function | Description | Comment |
+| --- | --- | --- | --- |
+| MatchCreateTests |  |  |  |
+|  | setUp | create testcredentials | base testdata to be used in test |
+
 ---
--- guns/tests.py
+++ guns/tests.py
@@ -1,3 +1,116 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Gun
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+import io
 
-# Create your tests here.
+
+# Testing CRUD functionality, as well as some permissions
+# for the Gun App
+
+class GunCreateTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shooter11', password='password1234')
+        self.client.login(username='shooter11', password='password1234')
+
+    def test_create_gun(self):
+        url = '/guns/'
+        data = {
+            'brand': 'Glock',
+            'gun_model': 'G17',
+            'type': 'Handgun',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Gun.objects.count(), 1)
+        self.assertEqual(Gun.objects.get().brand, 'Glock')
+        # print(response.data)
+
+    def test_create_gun_with_large_image(self):
+        # if if large images cannot be used, since we have the
+        # limitation in place in the serializer
+        url = '/guns/'
+        image = Image.new('RGB', (5000, 5000))
+        tmp_file = io.BytesIO()
+        image.save(tmp_file, format='JPEG')
+        tmp_file.seek(0)
+        image_file = SimpleUploadedFile(
+            'large_image.jpg', tmp_file.read(), content_type='image/jpeg')
+
+        data = {
+            'brand': 'Glock',
+            'gun_model': 'G17',
+            'type': 'Handgun',
+            'image': image_file,
+        }
+        response = self.client.post(url, data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('Image with width larger than 4096px',
+                      str(response.data))
+
+
+class GunRetrieveTests(APITestCase):
+    # check if logged in user can retreive a gun object
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shooter11', password='password1234')
+        self.gun = Gun.objects.create(
+            owner=self.user, brand='Glock', gun_model='G17', type='Handgun')
+
+    def test_retrieve_gun(self):
+        # login user first
+        self.client.login(username='shooter11',
+                          password='password1234')
+        url = f'/guns/{self.gun.id}/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['brand'], 'Glock')
+
+    def test_unauthenticated_user_cannot_retrieve(self):
+        # Attempt to retrieve gun object without logging in
+        url = f'/guns/{self.gun.id}/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class GunUpdateTests(APITestCase):
+    # check if logged in user can update a gun object
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shooter11', password='password1234')
+        self.gun = Gun.objects.create(
+            owner=self.user, brand='Glock', gun_model='G17', type='Handgun')
+        # user needs to be logged in
+        self.client.login(username='shooter11',
+                          password='password1234')
+
+    def test_update_gun(self):
+        # update brand and model
+        url = f'/guns/{self.gun.id}/'
+        data = {'brand': 'SIG', 'gun_model': 'P320'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.gun.refresh_from_db()
+        self.assertEqual(self.gun.brand, 'SIG')
+        self.assertEqual(self.gun.gun_model, 'P320')
+
+
+class GunDeleteTests(APITestCase):
+    # check if logged in user can delete a gun oject
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='shooter11', password='password1234')
+        self.gun = Gun.objects.create(
+            owner=self.user, brand='SIG', gun_model='P365', type='Handgun')
+        # login user
+        self.client.login(username='shooter11',
+                          password='password1234')  # User logged in
+
+    def test_delete_gun(self):
+        url = f'/guns/{self.gun.id}/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Gun.objects.count(), 0)
